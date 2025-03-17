b829f52fdcd9f1d0ead1792eb2de6d223b7a92a9
fagja <hey.shunya19@gmail.com>
2025-03-17 19:49:05 +0900

    feat: PageContainerコンポーネントの追加とロードマップの更新

--- src/components/layout/page-container.tsx
+++ src/components/layout/page-container.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { cn } from '@/lib/utils';
 
 interface PageContainerProps {
   children: React.ReactNode;
+  className?: string;
+  fullWidth?: boolean;
 }
 
 /**
  * 全ページ共通のコンテナコンポーネント
  * 一貫したパディングとマージンを適用します
+ * レスポンシブ対応済み
  */
-export function PageContainer({ children }: PageContainerProps) {
+export function PageContainer({
+  children,
+  className,
+  fullWidth = false
+}: PageContainerProps) {
   return (
-    <div className="mx-auto max-w-md w-full px-4 pb-6 pt-2 min-h-[calc(100vh-2rem)]">
+    <div
+      className={cn(
+        "mx-auto w-full px-4 pb-6 pt-2 min-h-[calc(100vh-2rem)]",
+        !fullWidth && "max-w-md sm:max-w-lg md:max-w-xl",
+        "sm:px-6 md:px-8",
+        className
+      )}
+    >
       {children}
     </div>
   );
