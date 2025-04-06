d7a4a7d7e68be86e066a063ac32835222d6aeeac
fagja <hey.shunya19@gmail.com>
2025-04-06 11:56:22 +0900

    change likes delete policy

--- supabase/migrations/20250406021806_add_likes_delete_policy.sql
+++ supabase/migrations/20250406021806_add_likes_delete_policy.sql
@@ -1,7 +1,7 @@
 -- supabase/migrations/20250406021806_add_likes_delete_policy.sql
 ALTER TABLE public.likes ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY "Allow authenticated users to delete likes"
+CREATE POLICY "全ユーザーがいいね削除可能"
 ON public.likes FOR DELETE
-TO authenticated
+TO public
 USING (true);
