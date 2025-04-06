5469a6c67d30393ba4bc53d41800912bed64bc20
fagja <hey.shunya19@gmail.com>
2025-04-06 12:35:56 +0900

    adopt vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { PageContainer } from "@/components/layout/page-container";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({
   subsets: ["latin"],
